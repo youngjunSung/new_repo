8af73e72fde3dae540dd2cbbb01fa1db7acf6264
성영준 사원 <juneo@tmon.co.kr>
2021-04-14 02:06:53 +0900

    gulp 수정

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ var spritesmith = require('gulp.spritesmith');
 var sourcemaps = require('gulp-sourcemaps');
 var fileinclude = require('gulp-file-include');
 
-// gulp.task('default', ['fileinclude','sass']);
+// gulp.task('default', ['fileinclude']);
+
+// gulp.task('watch',function(){
+//   gulp.watch('src/scss/**/*.scss',['sass']);
+// });
 
 gulp.task('sass',function(){
   return gulp.src('src/scss/**/*.scss')
@@ -12,10 +16,6 @@ gulp.task('sass',function(){
   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
   .pipe(sourcemaps.write('.',{includeContent: true}))
   .pipe(gulp.dest('dist/css/'));
-});
-
-gulp.task('watch',function(){
-  gulp.watch('src/scss/**/*.scss',['sass']);
 });
 
 gulp.task('sprite', function() {
@@ -44,16 +44,16 @@ gulp.task('sprite', function() {
 });
 
 gulp.task('fileinclude', function(done) {
-    gulp.src(['./src/html/index.html', './src/html/include/*.html'], {base : './src/html'})
+    gulp.src(['./src/html/index.html'], {base : './src/html'})
         .pipe(fileinclude({
             prefix: '@@',
-            basepath: 'src/html/include/*.html'
+            basepath: 'src/html/include/'
         }))
     .pipe(gulp.dest('./dist/html'));
     done();
 });
 
-gulp.task(copyimg, function() {
-    return gulp.src(['src/img/**/*.{png,jpg,gif,svg,json}','src/img/*.{png,jpg,gif,svg,json}'])
+gulp.task('copyimg', function() {
+    return gulp.src(['./src/img/**/*.{png,jpg,gif,svg,json}','./src/img/*.{png,jpg,gif,svg,json}'])
         .pipe(gulp.dest('./dist/img'));
 });
